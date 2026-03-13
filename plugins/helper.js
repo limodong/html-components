172c92fac2f81f700e6d59d127de5549d4be6f33
limodong <1710109384@qq.com>
2026-03-13 17:56:24 +0800

    封装插件，插件未完成

---
+++ plugins/helper.js
@@ -0,0 +1,8 @@
+var plugin = {};
+plugin.mixin = function () { }
+plugin.clone = function (obj, deep) {
+
+}
+plugin.debounce = function () { }
+plugin.throttle = function () { }
+plugin.curry = function () { }
